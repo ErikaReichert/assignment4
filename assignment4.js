a187226b810d8473ab4273e4d8ea0d953cbdfec4
ErikaReichert <emr74@pitt.edu>
2016-11-28 21:38:10 -0500

    Update assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -1,10 +1,8 @@
-// The anonymous function below will fire on page load
-
 (function() {
-  // Magic!
+
   console.log('Keepin\'n it clean with an external script!');
 
-  var $url    = $('.flexsearch-input') || '';
+  var $url    = $('.flexsearch-input').val() || 'http://www.mattbowytz.com/simple_api.json?data=all';
 
   $('#mainForm').on('submit', function(e) {
     e.preventDefault();
@@ -12,9 +10,24 @@
       url: $url,
       method: 'GET'
     }).success(function(data) {
-      console.log(data);
+      var list = '';
+      $.each(data.data.interests, function(i, x) { list += '<li>' + x + '</li>';
+
+      });
+
+      console.log(list);
+      $('.URLresults').append(list);
+
+      var list = '';
+      $.each(data.data.programming, function(i, x) { list += '<li>' + x + '</li>';
+
+      });
+
+      console.log(list);
+      $('.URLresults2').append(list);
+
     }).fail(function(data) {
       console.log(data);
-    });
-  });
+      });
+     });
 })();
